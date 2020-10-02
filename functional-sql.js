0fb3dcdbf32cf7e9652aa94046a1af754ec5c444
iagokrt <iago_floripa@hotmai.com>
2020-10-02 14:00:07 -0300

    groupbyfn

--- functional-sql.js
+++ functional-sql.js
@@ -1,3 +1,19 @@
+function groupBy(executeData, groupByFns) {
+  if (!groupByFns.length) {
+    return executeData;
+  }
+  const groupByFn = groupByFns.shift();
+  const grouped = executeData.reduce((grouped, item) => {
+    const key = groupByFn(item);
+    grouped[key] = grouped[key] // [];
+    grouped[key].push(item);
+    return grouped;
+  }, {});
+  return Object.entries(grouped).map(([groupName, values]) => {
+    return [ groupName, groupBy(values, groupByFns.slice()) ];
+  });
+}
+
 class Query {
   constructor() {
     this.data = [];
@@ -15,13 +31,19 @@ class Query {
     this.whereFn = fn;
     return this;
   }
+  groupBy(...fns) {
+    this.groupByFns = fns;
+    return this;
+  }
+
   execute() {
     let executeData = this.data.slice();
-
     if (typeof this.whereFn === 'function') {
       executeData = executeData.filter(this.whereFn);
     }
-
+    if (this.groupByFns && this.groupByFns.length) {
+      executeData = groupBy(executeData, this.groupByFns.slice());
+    }
     if (typeof this.selectFn === 'function') {
       executeData = executeData.map(this.selectFn);
     }
