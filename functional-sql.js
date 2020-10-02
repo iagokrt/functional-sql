cc2af1dec190bb711daa5fb523cf1325b9cc3df7
iagokrt <iago_floripa@hotmai.com>
2020-10-02 04:09:02 -0300

    test assert passing

--- functional-sql.js
+++ functional-sql.js
@@ -1,19 +1,38 @@
 class Query {
-  select() {
+  constructor() {
+    this.data = [];
+  }
+  
+  select(fn) {
+    this.selectFn = fn;
     return this;
   }
   from(data) {
     this.data = data;
+    return this; 
+  }
+  where(fn) {
+    this.whereFn = fn;
     return this;
+  }
+  execute() {
+    let executeData = this.data.slice();
+
+    if (typeof this.whereFn === 'function') {
+      executeData = executeData.filter(this.whereFn);
+    }
+
+    if (typeof this.selectFn === 'function') {
+      executeData = executeData.map(this.selectFn);
+    }
+    return executeData;
   }
 }
 
 var query = function() {
-  // select propertie
-  return {
-    select: () => {}
-  };
-  
+  // select propertie: within instance
+  return new Query();
+
 };
 
 module.exports = query;
